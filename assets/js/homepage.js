1338b77bfb96d0f11826f035c9b8e6d14213dfd3
michmash2010 <michellecrowe2010@gmail.com>
2021-10-02 04:59:15 -0400

    add language buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,24 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                response.json()
+                    .then(function(data) {
+                        console.log(data)
+                        displayRepos(data.items,language);
+                    });
+            } else {
+                alert('Error: GitHumb User Not Found');
+            }
+        });
+};
 
 var formSubmitHandler = function(event) {
     // prevent page from refreshing
@@ -98,7 +116,19 @@ var displayRepos = function(repos, searchTerm) {
     }
 };
 
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    console.log(language);
+    if (language) {
+        getFeaturedRepos(language);
+      
+        // clear old content
+        repoContainerEl.textContent = "";
+      }
+};
 
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
 
